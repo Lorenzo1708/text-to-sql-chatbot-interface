183191988731a0f62dd87be147d205ca33ab11ab
Lorenzo <lorenzo2846@outlook.com>
2025-08-31 14:45:29 -0300

    feat: Add model dropdown

--- app/main.py
+++ app/main.py
@@ -1,20 +1,31 @@
 import os
+from typing import Literal
 
 import gradio as gr
 import requests
 
 
-def chat(message: str, history: list[dict[str, str | None]]) -> str:
+def chat(
+    message: str,
+    history: list[dict[str, str | None]],
+    model: Literal["gemini-2.5-flash", "gemini-2.5-pro"],
+) -> str:
     url = os.getenv("URL")
 
     if not url:
         raise Exception("if not URL")
 
     return requests.post(
-        f"{url}/api/v1/chat", json={"message": message, "history": history}
+        f"{url}/api/v1/chat",
+        json={"model": model, "message": message, "history": history},
     ).json()["content"]
 
 
-chat_interface = gr.ChatInterface(chat, type="messages", save_history=True)
+chat_interface = gr.ChatInterface(
+    chat,
+    type="messages",
+    additional_inputs=[gr.Dropdown(["gemini-2.5-flash", "gemini-2.5-pro"])],
+    save_history=True,
+)
 chat_interface.saved_conversations.secret = "secret"
 chat_interface.launch()
